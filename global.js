fdbcde992f7189fe90a0047458e1946669a9cfea
DENJI-54 <Denji8005@gmail.com>
2025-02-08 12:52:54 +0530

    Add movies and likes route

--- global.js
+++ global.js
@@ -1,41 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
-import Movies from "../models/moviesModel.js";
-import Users from "../models/usersModel.js";
+import { getAllMovies, getMovieById , hii} from "../controllers/global.js";
+const app = express();
+const global_router = express.Router();
 
+app.use("movies", global_router);
+global_router.get('/hi',hii)
+global_router.get("/all", getAllMovies); //working
+global_router.get("/user/:id", getMovieById); //working
 
-const getAllMovies = async (req, res) => {
-  try {
-    const result = await Movies.find();
-    console.log(`All moviesMoviess ${result}`);
-    res.json({
-      movies: result
-    })
-  } catch (error) {
-    console.log(`Error getting movies ${error}`);
-  }
-};
-
-const getMovieById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const reslut = await Users.findById(id).populate({
-      path: "movies",
-      select: ["title","ratings","desc","genre"]
-    });
-    console.log(id)
-    return res.status(200).json({
-      movie: reslut,
-    });
-  } catch (error) {
-    res.status(400).json({ "error getting user ": error });
-    console.log(`Error find user with the given id : ${error}`);
-  }
-};
-
-const hii = (req,res) =>{
-  console.log(`started`)
-  res.send(`hiii from linux mx xfce`)
-};
-
-export { getAllMovies, getMovieById , hii };
+export default global_router;
